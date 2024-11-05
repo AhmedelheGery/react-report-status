048edc682db8653a662c7edd14599af84f9c9787
Ahmed Alaa <ahmed.3laa.2020@gmail.com>
2024-11-05 18:00:47 +0200

    fix: remvng public key value

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,7 @@ const BASE_URL = 'https://sandbox.uberall.com/api';
 
 const HEADERS = {
   'Content-Type': 'application/json',
-  publicKey:
-    '7a9cccda8e21b961f9cdd7c822e12d27fd68bcac40eee033f2a2eb41c0985a35cd3a8c8aca9fd8a6a0fa7beca434a70775389af9375768ff54f7b46ed7a740e5',
+  publicKey: 'USE_YOUR_PUBLIC_KEY',
 };
 
 export const search = async (params) => {
